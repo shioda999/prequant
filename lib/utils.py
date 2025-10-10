ea421cd8e123f87788c07924c1219ed243b7b104
shioda999 <k0shig0e17@gmail.com>
2025-10-11 00:48:38 +0900

    a

--- lib/utils.py
+++ lib/utils.py
@@ -119,7 +119,7 @@ def q_err(m, nbits=4, sz=32, scale=None, act_scale=None, t=False, H=None, o_shri
         delta = delta.mul(scale.weight if hasattr(scale, "weight") else scale)
     if act_scale is not None:
         # pass
-        delta = delta.mul(act_scale.to(delta.device).pow(0.3))
+        delta = delta.mul(act_scale.to(delta.device).pow(0.5))
     if t is True:
         delta = delta.T
     if H is not None:
