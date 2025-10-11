835a44d6c584655b7d95137a92dcc0c9392ad682
shioda999 <k0shig0e17@gmail.com>
2025-10-11 18:12:18 +0900

    a

--- lib/smooth.py
+++ lib/smooth.py
@@ -170,7 +170,7 @@ def smooth_fn_pow(As, Bs, a=None, b=None, device=None, chunk_size=32):
             new_loss = compute_loss(r.pow(i))
             p = torch.where(new_loss < loss, i, p)
             loss = torch.minimum(new_loss, loss)
-        return r.pow(p[:,None].expand(-1, chunk_size).reshape(-1)), loss
+        return r.pow(p[:,None].clamp(0.,5.)).expand(-1, chunk_size).reshape(-1)), loss
 
     p = 2
     r = 1 / torch.concat([normalize(A.weight)[..., None] for A in As], dim=-1).reshape(As[0].weight.shape[0], -1).abs().pow(p).mean(dim=1).pow(1/p)
@@ -178,14 +178,13 @@ def smooth_fn_pow(As, Bs, a=None, b=None, device=None, chunk_size=32):
     
     s, loss = calc_minimum_loss(r)
     s2, loss2 = calc_minimum_loss(r2)
-
     s = torch.where((loss < loss2)[:,None].expand(-1, chunk_size).reshape(-1), s, s2)
     loss = torch.where(loss < loss2, loss, loss2)
 
-    if hasattr(As[0], "act_scale"):
-        s2, loss2 = calc_minimum_loss(As[0].act_scale)
-        s = torch.where((loss < loss2)[:,None].expand(-1, chunk_size).reshape(-1), s, s2)
-        loss = torch.where(loss < loss2, loss, loss2)
+    # if hasattr(As[0], "act_scale"):
+    #     s2, loss2 = calc_minimum_loss(As[0].act_scale)
+    #     s = torch.where((loss < loss2)[:,None].expand(-1, chunk_size).reshape(-1), s, s2)
+    #     loss = torch.where(loss < loss2, loss, loss2)
 
     if hasattr(Bs[0], "act_scale"):
         s2, loss2 = calc_minimum_loss(1 / Bs[0].act_scale)
