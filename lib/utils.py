e0866c2e95d4e11ae14ad21fb045e8f47b4c8885
shioda999 <k0shig0e17@gmail.com>
2025-10-14 00:02:29 +0900

    a

--- lib/utils.py
+++ lib/utils.py
@@ -121,6 +121,7 @@ def q_err(m, nbits=4, sz=32, scale=None, act_scale=None, t=False, H=None, o_shri
         delta2 = delta
     if act_scale is not None:
         delta = delta.mul(act_scale.to(delta.device))
+        delta2.mul_(act_scale.to(delta.device).mean())
     if t is True:
         delta = delta.T
         delta2 = delta2.T
