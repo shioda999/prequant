0c0a5ae73f7e35fde6b8357753da4e991b1083f7
shioda999 <k0shig0e17@gmail.com>
2025-10-18 17:19:38 +0900

    a

--- lib/smooth.py
+++ lib/smooth.py
@@ -158,12 +158,13 @@ def smooth_fn_greedy(As, Bs, n_iterations=500, device=None, chunk_size=32, step_
     for B in Bs: B.cpu()
 
 @torch.no_grad()
-def smooth_fn_pow(As, Bs, device=None, chunk_size=32):
+def smooth_fn_pow(As, Bs, device=None, chunk_size=32, importance=None):
     if device is None: device = get_device()
     for A in As: A.to(device)
     for B in Bs: B.to(device)
     # Bs_scale = [B.weight.float().abs().mean() for B in Bs]
-    Bs_scale = [B.weight.float().pow(2).mean().sqrt() for B in Bs]
+    if importance is None: importance = [10 if i == len(Bs) - 1 else 1 for i in range(len(Bs))]
+    Bs_scale = [B.weight.float().pow(2).mean().sqrt() * imp for B, imp in zip(Bs, importance)]
     for B, s in zip(Bs, Bs_scale): B.weight.div_(s)
     
     dim = Bs[0].weight.shape[-1]
