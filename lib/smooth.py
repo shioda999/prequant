2af433fd061383133bfffcc2f3f8b25b1a9136fa
shioda999 <k0shig0e17@gmail.com>
2025-10-19 17:20:05 +0900

    a

--- lib/smooth.py
+++ lib/smooth.py
@@ -105,7 +105,7 @@ def quantization_loss_for_smooth(As, Bs, num_chunks, H, s):
         sa = torch.concat([A.weight[..., None] for A in As], dim=-1).reshape(As[0].weight.shape[0], -1).abs().pow(2).mean(dim=1).pow(0.5)
         for i, B in enumerate(Bs):
             hamiltonian = getattr(B, "H", None)
-            loss += q_err(B.weight / s, scale=s * sa, act_scale=As[0].act_scale.sqrt(), o_shrink=False, H=H, hamiltonian=hamiltonian).reshape(-1, B.weight.shape[-1]).sum(dim=0)
+            loss += q_err(B.weight / s, scale=s * sa, act_scale=(B.act_scale/sa).sqrt(), o_shrink=False, H=H, hamiltonian=hamiltonian).reshape(-1, B.weight.shape[-1]).sum(dim=0)
             # loss += q_err(B.weight / s, scale=s, act_scale=B.act_scale, o_shrink=False, H=H, hamiltonian=hamiltonian).reshape(-1, B.weight.shape[-1]).sum(dim=0)
     else:
         sa = torch.concat([A.weight[..., None] for A in As], dim=-1).reshape(As[0].weight.shape[0], -1).abs().pow(2).mean(dim=1).pow(0.5)
