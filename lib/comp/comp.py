e75d045890326b6a45a582f628ce7442efaab2b3
shioda999 <k0shig0e17@gmail.com>
2025-10-24 00:14:33 +0900

    a

--- lib/comp/comp.py
+++ lib/comp/comp.py
@@ -18,9 +18,11 @@ def compress(model, nbits=4, group_sz=32, **kwargs):
         w = torch.round(w.sub(min_v).div(s)).clamp(0, Qp).reshape(shape) - 8
     comp = LoRAStackCompressor.from_weights(w, **kwargs)
     # comp = CrossLayerAECompressor.from_weights(W, **kwargs)
-    w_rec = comp().cpu()
-    mse = torch.mean((w_rec - w) ** 2).item()
+    w_rec = comp()
+    mse = torch.mean((w_rec - w.to(w_rec.device)) ** 2).item()
     print(f"final mse: {mse:.6e}")
+
+    w_rec = w_rec.reshape(-1, group_sz).add(8).mul(s).add(min_v).reshape(shape).to(dtype).cpu()
     
     for i, l in enumerate(layers):
-        get_down(l).weight.data = w_rec[i].reshape(-1, group_sz).add(8).mul(s).add(min_v).reshape(shape).to(dtype)
+        get_down(l).weight.data = w_rec[i]
