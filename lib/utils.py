51ac819596a889b74fabbf9719455b9c26799250
shioda999 <k0shig0e17@gmail.com>
2025-12-07 00:10:59 +0900

    a

--- lib/utils.py
+++ lib/utils.py
@@ -192,7 +192,7 @@ def q_err(m, nbits=4, sz=32, scale=None, act_scale=None, t=False, H=None, o_shri
     if scale is not None:
         delta = delta.mul(scale.weight if hasattr(scale, "weight") else scale)
         delta2 = delta
-    if hamiltonian is not None and t is False:
+    if False: # hamiltonian is not None and t is False:
         h = block_diagonalize(hamiltonian).to(delta.dtype)
         loss = (delta @ h * delta).mean(dim=0)
         # loss = (delta @ h * delta).sum(dim=-1, keepdim=True).pow(2).mean(dim=0)
