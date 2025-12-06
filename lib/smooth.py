5791eb58ea6ce0fcd92eafac07ac88c5ca1d4263
shioda999 <k0shig0e17@gmail.com>
2025-12-06 21:10:25 +0900

    a

--- lib/smooth.py
+++ lib/smooth.py
@@ -201,10 +201,10 @@ def smooth_fn_pow(As, Bs, device=None, chunk_size=32, importance=None, ignore_ac
     if len(Bs) == 3:
         print("qkv", loss.mean().item())
 
-    if hasattr(As[0], "act_o_scale") and ignore_act_scale is False:
-        s2, loss2 = calc_minimum_loss(1 / As[0].act_o_scale)
-        s = torch.where((loss < loss2)[:,None].expand(-1, chunk_size).reshape(-1), s, s2)
-        loss = torch.where(loss < loss2, loss, loss2)
+    # if hasattr(As[0], "act_o_scale") and ignore_act_scale is False:
+    #     s2, loss2 = calc_minimum_loss(1 / As[0].act_o_scale)
+    #     s = torch.where((loss < loss2)[:,None].expand(-1, chunk_size).reshape(-1), s, s2)
+    #     loss = torch.where(loss < loss2, loss, loss2)
 
     # w_b = torch.concat([B.weight for B in Bs]).float()
     # shape = w_b.shape
